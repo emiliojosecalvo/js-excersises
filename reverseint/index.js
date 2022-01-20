20f307036046c24364b11d9fab45fb9e5a27cd35
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-20 22:18:14 +0000

    my sol reverseint

---
+++ reverseint/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+    let newNumber = n * Math.sign(n);
+    let result = '';
+
+    for (let c of newNumber.toString()) {
+        if (c != 0) {
+            result = c + result
+        }
+    }
+    result = result * Math.sign(n);
+    return result
+}
+
+module.exports = reverseInt;
