cffee03b4f6b29a81cb677f418bf210a48adff37
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-20 18:50:19 +0000

    first commit

---
+++ reversestring/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+
+
+reverse('hola');
+
+module.exports = reverse;
+
+//First Solution
+// function reverse(str) {
+//     //first we split the str into an array
+//     let array = str.split('');
+//     //here we reverse all positions from the array
+//     array.reverse('');
+//     //here we join them as a single string
+//     return array.join('');
+// }
