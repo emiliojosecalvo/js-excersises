12542f6992c348f421407a14ac7873e435a00dcd
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-20 21:41:45 +0000

    second sol polindrome

--- palindrome/index.js
+++ palindrome/index.js
@@ -8,13 +8,28 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    let reverseStr = '';
-    // reverse the word
-    for (let c of str) {
-        reverseStr = c + reverseStr;
-    }
-    //compare the reversed word with the original
-    return reverseStr === str;
+    return str.split('').every((character, index) => {
+        return character === str[str.length - index - 1];
+    });
 }
 
+
 module.exports = palindrome;
+
+// First Solution
+// function palindrome(str) {
+//     let reverseStr = '';
+//     // reverse the word
+//     for (let c of str) {
+//         reverseStr = c + reverseStr;
+//     }
+//     //compare the reversed word with the original
+//     return reverseStr === str;
+// }
+
+// Second Solution
+// function palindrome(str) {
+//     return str.split('').every((character, index) => {
+//         return character === str[str.length - index - 1];
+//     });
+// }
