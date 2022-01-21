e4775035c4d2bd7b48bb541a26e7464238efbb5b
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-21 00:40:53 +0000

    cleaner sol fizzbuzz

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -13,9 +13,18 @@
 //   buzz
 
 function fizzBuzz(n) {
-
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz')
+        } else if (i % 3 === 0) {
+            console.log('fizz')
+        } else if (i % 5 === 0) {
+            console.log('buzz')
+        } else {
+            console.log(i)
+        }
+    }
 }
-
 
 
 module.exports = fizzBuzz;
@@ -35,4 +44,14 @@ module.exports = fizzBuzz;
 //             console.log(num)
 //         }
 //     }
+// }
+
+//how to get if a number is divisible by other
+// variable a = 10;
+// variable b = 5;
+
+// if ((a % b) == 0) {
+//     // a is divisible by b
+// else {
+//     //a is not divisible by b
 // }
